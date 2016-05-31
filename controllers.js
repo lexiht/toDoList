3d11444d332c0bf256455ec6b1ea4024e7bb6f7c
Lexi Tran <hazukitran@gmail.com>
2016-05-31 22:33:45 +0700

    To do list app

---
+++ controllers.js
@@ -0,0 +1,13 @@
+toDoApp.controller('ToDoController', ['ToDoService', 'ToDoFactory', function(ToDoService, ToDoFactory){
+	var self = this;
+
+	self.todos = ToDoService.getAll();
+
+	self.addToDo = function(todoText) {
+		self.todos.push(new ToDoFactory(todoText));
+	};
+
+	self.removeToDo = function() {
+		self.todos.pop();
+	};
+}]);
